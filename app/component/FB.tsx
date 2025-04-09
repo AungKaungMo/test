7c8378a0ac3f33382be6de0580a1871f9e94a674
AungKaungMoeOway <aungkaungmoe@owaytrip.com>
2025-04-09 20:27:05 +0630

    first

--- app/component/FB.tsx
+++ app/component/FB.tsx
@@ -11,22 +11,68 @@
 
 // export default FB
 
+
 "use client"
 import { useEffect } from 'react';
 
-const FB = () => {
+// Declare FB types globally
+declare global {
+  interface Window {
+    fbAsyncInit: () => void;
+    FB: {
+      init: (config: {
+        xfbml: boolean;
+        version: string;
+      }) => void;
+    };
+  }
+}
+
+const FBChat = () => {
+  useEffect(() => {
+    // Initialize Facebook SDK
+    window.fbAsyncInit = function() {
+      window.FB.init({
+        xfbml: true,
+        version: 'v18.0'
+      });
+    };
+
+    // Load Facebook SDK script
+    (function(d, s, id) {
+      const fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      const js = d.createElement(s) as HTMLScriptElement;
+      js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+      js.async = true;
+      js.defer = true;
+      if (fjs.parentNode) {
+        fjs.parentNode.insertBefore(js, fjs);
+      }
+    }(document, 'script', 'facebook-jssdk'));
+
+    return () => {
+      // Cleanup if needed
+      // delete window.fbAsyncInit;
+      // delete window.FB;
+    };
+  }, []);
+
   return (
-    <iframe
-      src={`https://www.facebook.com/v18.0/plugins/customerchat.php?page_id=530863376787158&minimized=true`}
-      width="350"
-      height="500"
-      style={{border: 'none', overflow: 'hidden'}}
-      scrolling="no"
-      frameBorder="0"
-      allow="encrypted-media"
-      allowFullScreen={true}
-    />
+    <>
+      <div id="fb-root"></div>
+      <div 
+        className="fb-customerchat"
+        data-attribution="setup_tool"
+        data-page-id="530863376787158"
+        data-theme-color="#0084ff"
+        data-logged-in-greeting="Hi! How can we help you?"
+        data-logged-out-greeting="Hi! How can we help you?"
+        data-minimized="true"
+      ></div>
+    </>
   );
 }
 
-export default FB;
+export default FBChat;
