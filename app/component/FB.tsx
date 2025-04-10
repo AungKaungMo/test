f434f3c5b45b8b417329151bd0f5df29099b525a
AungKaungMoeOway <aungkaungmoe@owaytrip.com>
2025-04-10 20:50:55 +0630

    fi

--- app/component/FB.tsx
+++ app/component/FB.tsx
@@ -10,58 +10,12 @@
 // }
 
 // export default FB
+import { MessengerChat } from "react-messenger-chat-plugin";
 
-"use client";
-
-import { useRef, useEffect } from "react";
-declare global {
-  interface Window {
-    fbAsyncInit: Function;
-    FB: any;
-  }
+const FB = () => {
+  return (
+    <MessengerChat pageId='530863376787158' />
+  )
 }
 
-const ChatBot: React.FC = () => {
-  const MessengerRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (MessengerRef.current) {
-      // Set page_id and attribution as attributes on the MessengerRef
-      MessengerRef.current.setAttribute("page_id", "530863376787158");
-      MessengerRef.current.setAttribute("attribution", "biz_inbox");
-
-      // Initialize the Facebook SDK
-      window.fbAsyncInit = function () {
-        window.FB.init({
-          xfbml: true,
-          version: "v16.0",
-        });
-      };
-
-      // Load Facebook SDK script dynamically
-      (function (d, s, id) {
-        var js: any,
-          fjs : any = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-        fjs.parentNode.insertBefore(js, fjs);
-      })(document, "script", "facebook-jssdk");
-    }
-  }, []);
-
-  return (
-    <>
-      <div id="fb-root"></div>
-      {/* Messenger chat plugin */}
-      <div
-        ref={MessengerRef}
-        id="fb-customer-chat"
-        className="fb-customerchat"
-      ></div>
-    </>
-  );
-};
-
-export default ChatBot;
+export default FB
